ed60ad30ad4dc863a0efae1c7e987eff269a26d0
lorex <lorex59@mail.ru>
2023-01-12 17:49:58 +1000

    add RoomDataBase and TaskViewModel

---
+++ app/src/main/java/com/example/fakegoogletasks/database/TaskDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.fakegoogletasks.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.fakegoogletasks.database.dao.TaskDao
+import com.example.fakegoogletasks.entity.Task
+
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class TaskDatabase : RoomDatabase() {
+
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "user_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/fakegoogletasks/viewmodels/TaskViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.fakegoogletasks.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.fakegoogletasks.entity.Task
+
+class TaskViewModel(application: Application): AndroidViewModel(application) {
+
+
+
+
+}
